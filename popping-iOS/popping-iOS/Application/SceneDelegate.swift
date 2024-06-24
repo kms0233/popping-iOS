1ed936f757f635ff899db5b8bbc08c6acb96db9c
kms0233 <cnblue789@naver.com>
2024-06-24 19:01:56 +0900

    [Setting] 프로젝트 초기 세팅

---
+++ popping-iOS/popping-iOS/Application/SceneDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  SceneDelegate.swift
+//  popping-iOS
+//
+//  Created by 김민서 on 6/24/24.
+//
+
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        // 1.
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+    // 2.
+        self.window = UIWindow(windowScene: windowScene)
+    // 3.
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        self.window?.rootViewController = navigationController
+    // 4.
+        self.window?.makeKeyAndVisible()
+    }
+}
