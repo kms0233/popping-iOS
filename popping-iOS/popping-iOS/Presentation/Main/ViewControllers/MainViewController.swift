b1225c12bd9046f9427d004fe0f34b22d96674fc
kms0233 <cnblue789@naver.com>
2024-07-25 18:04:50 +0900

    [Feat] View와 ViewController 분리 (#5)

--- popping-iOS/popping-iOS/Presentation/Main/ViewControllers/MainViewController.swift
+++ popping-iOS/popping-iOS/Presentation/Main/ViewControllers/MainViewController.swift
@@ -15,8 +15,8 @@ final class MainViewController: UIViewController, UICollectionViewDelegate {
     
     // MARK: - UI Properties
     
-    private lazy var mainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.makeFlowLayout())
-    private let navigationBarView = NavigationBarView()
+    private let mainView = MainView()
+    
     
     private var mainData: [Contents] = Contents.posterImages.map { Contents(image: $0, title: "", location: "", date: "") }
     private var recommendData: [Contents] = Contents.recommendDummyContents()
@@ -39,37 +39,28 @@ final class MainViewController: UIViewController, UICollectionViewDelegate {
     // MARK: - Private Methods
     
     private func setHierarchy() {
-        self.view.addSubviews(mainCollectionView, navigationBarView)
+        self.view.addSubview(mainView)
     }
     
     private func setLayout() {
-        mainCollectionView.snp.makeConstraints {
-            $0.top.equalTo(navigationBarView.snp.bottom)
-            $0.bottom.trailing.leading.equalToSuperview()
-        }
-        navigationBarView.snp.makeConstraints {
-            $0.top.equalToSuperview()
-            $0.width.equalToSuperview()
-            $0.height.equalTo(108)
+        
+        mainView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
     
     private func setStyle() {
         self.navigationController?.navigationBar.isHidden = true
-        mainCollectionView.do {
-            $0.backgroundColor = .white
-            $0.showsVerticalScrollIndicator = false
-            $0.contentInsetAdjustmentBehavior = .never
-        }
+ 
     }
     
     private func setDelegate() {
-        mainCollectionView.delegate = self
-        mainCollectionView.dataSource = self
+        mainView.mainCollectionView.delegate = self
+        mainView.mainCollectionView.dataSource = self
     }
     
     private func registerCell() {
-        mainCollectionView.do {
+        mainView.mainCollectionView.do {
             $0.register(MainPosterCell.self, forCellWithReuseIdentifier: MainPosterCell.identifier)
             $0.register(RecommendCell.self, forCellWithReuseIdentifier: RecommendCell.identifier)
             $0.register(HeaderView.self, forSupplementaryViewOfKind: HeaderView.elementKinds, withReuseIdentifier: HeaderView.identifier)
@@ -77,65 +68,14 @@ final class MainViewController: UIViewController, UICollectionViewDelegate {
         }
     }
     
-    private func makeFlowLayout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout { section, _ -> NSCollectionLayoutSection? in
-            switch self.dataSource[section] {
-            case .mainContents: return self.makeMainContentsLayout()
-            case .recommendedContents, .deadlineContents: return self.makeRecommendLayout()
-            }
-        }
-    }
     
-    private func makeMainContentsLayout() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPaging
-        
-        let footer = makePageControlButtonView()
-        section.boundarySupplementaryItems = [footer]
-        
-        return section
-    }
-    
-    private func makeRecommendLayout() -> NSCollectionLayoutSection {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(140/375), heightDimension: .absolute(240))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        group.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0)
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .continuous
-        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0)
-        
-        let header = makeHeaderView()
-        section.boundarySupplementaryItems = [header]
-        return section
-    }
-    
-    private func makeHeaderView() -> NSCollectionLayoutBoundarySupplementaryItem {
-        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(25))
-        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: HeaderView.elementKinds, alignment: .top)
-        return header
-    }
-    
-    private func makePageControlButtonView() -> NSCollectionLayoutBoundarySupplementaryItem {
-        let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(15))
-        let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize, elementKind: PageControlButtonView.elementKinds, alignment: .bottom)
-        return footer
-    }
 }
 
 
 extension MainViewController: MainPosterDelegate {
     func didSwipePoster(index: Int, vc: UIPageViewController, vcData: [UIViewController]) {
         currentPage = index
-        if let pageControlButtonView = mainCollectionView.supplementaryView(forElementKind: PageControlButtonView.elementKinds, at: IndexPath(item: 0, section: 0)) as? PageControlButtonView {
+        if let pageControlButtonView = mainView.mainCollectionView.supplementaryView(forElementKind: PageControlButtonView.elementKinds, at: IndexPath(item: 0, section: 0)) as? PageControlButtonView {
             pageControlButtonView.index = currentPage
         }
     }
--- popping-iOS/popping-iOS/Presentation/Main/Views/MainView.swift
+++ popping-iOS/popping-iOS/Presentation/Main/Views/MainView.swift
@@ -12,6 +12,14 @@ import Then
 
 final class MainView: UIView {
 
+    // MARK: - UI Properties
+    
+    lazy var mainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: self.makeFlowLayout())
+    
+    private let navigationBarView = NavigationBarView()
+    
+    private let dataSource: [MainSection] = MainSection.dataSource
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -24,26 +32,89 @@ final class MainView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-   
-}
-
-
-// MARK: - Private Methods
-
-private extension MainView {
-    
     func setHierarchy() {
-        self.addSubviews()
+        self.addSubviews(mainCollectionView, navigationBarView)
     }
     
     func setLayout() {
-       
+        
+        mainCollectionView.snp.makeConstraints {
+            $0.top.equalTo(navigationBarView.snp.bottom)
+            $0.bottom.trailing.leading.equalToSuperview()
+        }
+        
+        navigationBarView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.width.equalToSuperview()
+            $0.height.equalTo(108)
+        }
     }
     
     func setStyle() {
         self.backgroundColor = .black
         
-  
+        mainCollectionView.do {
+            $0.backgroundColor = .white
+            $0.showsVerticalScrollIndicator = false
+            $0.contentInsetAdjustmentBehavior = .never
+        }
     }
+   
 }
 
+
+extension MainView {
+    
+    private func makeFlowLayout() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { section, _ -> NSCollectionLayoutSection? in
+            switch self.dataSource[section] {
+            case .mainContents: return self.makeMainContentsLayout()
+            case .recommendedContents, .deadlineContents: return self.makeRecommendLayout()
+            }
+        }
+    }
+    
+    private func makeMainContentsLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(300))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        let footer = makePageControlButtonView()
+        section.boundarySupplementaryItems = [footer]
+        
+        return section
+    }
+    
+    private func makeRecommendLayout() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 8)
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(140/375), heightDimension: .absolute(240))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        group.contentInsets = NSDirectionalEdgeInsets(top: 15, leading: 0, bottom: 0, trailing: 0)
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 0)
+        
+        let header = makeHeaderView()
+        section.boundarySupplementaryItems = [header]
+        return section
+    }
+    
+    private func makeHeaderView() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let headerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(25))
+        let header = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: headerSize, elementKind: HeaderView.elementKinds, alignment: .top)
+        return header
+    }
+    
+    private func makePageControlButtonView() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let footerSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .estimated(15))
+        let footer = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: footerSize, elementKind: PageControlButtonView.elementKinds, alignment: .bottom)
+        return footer
+    }
+}
